c70363223f13d9e4de6bd91cf17926560e6e1a97
BruceBarigye <ndyomukingbruce@gmail.com>
2022-04-25 17:51:12 +0300

    added some content in html and js files

--- dope.js
+++ dope.js
@@ -8,3 +8,25 @@ const person= {
 }
 
 console.log(`My first name is ${person.firstName}, my last name is ${person.lastName}, and my age is ${person.age}`)
+
+//functions
+//is a block of code that performs a specific task
+function Compliment(lang) {
+    console.log("Am getting better at " + lang)
+    console.log(`Am getting better at ${lang}`)
+}
+Compliment("JavaScript")
+
+// function ChangeGreeting() {
+//     const greeting = document.getElementById("greeting")
+//     greeting.textContent.add
+// }
+
+const SecondCompliment = function(lang) {
+    const school = "Isbat"
+    console.log(school)
+    console.log(`Am getting better at ${lang}`)
+}
+SecondCompliment("Python")
+console.log(school)
+
