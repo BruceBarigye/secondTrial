7baf6a3ada908cadf32186034a1a3617781ff41f
BruceBarigye <ndyomukingbruce@gmail.com>
2022-04-25 16:56:13 +0300

    added a js file in an html file

---
+++ dope.js
@@ -0,0 +1,10 @@
+const names= ["bruce", "zeph","Jacob"]
+console.log(names)
+
+const person= {
+    firstName: "Bruce",
+    lastName: "Barigye",
+    age: 24
+}
+
+console.log(`My first name is ${person.firstName}, my last name is ${person.lastName}, and my age is ${person.age}`)
